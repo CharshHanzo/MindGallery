0b64b9855685ad136b783a176544e3eed493203f
CharshHanzo <2837998267@qq.com>
2026-01-18 23:17:13 +0800

    feat(frontend):页面初始化

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -1,23 +1,8 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import axios from 'axios'
-
-const message = ref('')
-
-const testBackend = async () => {
-  try {
-    const res = await axios.get('http://localhost:3000/api/test')
-    message.value = res.data.message
-  } catch (error) {
-    message.value = `连接后端失败${error}`
-  }
-}
 </script>
 
 <template>
   <div>
-    <h1>图片记忆墙</h1>
-    <button @click="testBackend">测试后端连接</button>
-    <p>后端状态：{{ message }}</p>
+    <router-view />
   </div>
 </template>
--- packages/frontend/src/views/HomeView.vue
+++ packages/frontend/src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+import axios from 'axios'
+
+const message = ref('')
+
+const testBackend = async () => {
+  try {
+    const res = await axios.get('http://localhost:3000/api/test')
+    message.value = res.data.message
+  } catch (error) {
+    message.value = `连接后端失败${error}`
+  }
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>图片记忆墙</h1>
+    <button @click="testBackend">测试后端连接</button>
+    <p>后端状态：{{ message }}</p>
+  </div>
 </template>
